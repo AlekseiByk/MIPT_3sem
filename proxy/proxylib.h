5f10aeb637b346be848394776d6ddf525552c587
Aleksei <alexeibyk@mail.ru>
2020-12-18 16:09:17 +0300

    proxy

--- proxy/proxylib.h
+++ proxy/proxylib.h
@@ -10,22 +10,59 @@
 #include <signal.h>
 #include <sys/prctl.h>
 
+struct pipes_info
+{
+	int from_parent_fd[2];		//reciever pipe
+	int to_parent_fd[2];		//transmitter pipe
+	int child_pid;			//for wait
+};
 
-extern int transfer_bit;
-extern int old_ppid;
+struct info{
+	int    fd_wr;
+    int    fd_rd;
+    size_t buff_size;
+    char*  write_end;
+    char*  read_end;
+    char*  buff;
+    char*  end;
+    size_t empty;
+    size_t full;
+};
 
-void parent_func (pid_t cpid);
-void child_func (char *filename);
+int child_func (const char * filename, pipes_info* links, int number, int children_count);
+int parent_func (pipes_info* links, int children_count, int number_of_fd);
 
-void handler (int num);
-void sigchild (int num);
-void child_handler (int num);
-void child_hup (int num);
-
-
-void CheckError(int Expression, char * messege){
-	if (Expression){
+void CheckError(int Expression, const char * messege){
+	if (Expression){             
 		perror (messege);
 		exit (EXIT_FAILURE);
 	}
+}
+
+long take_one_positive_number_from_args (int argc, char** argv)
+{
+	char *strptr = NULL, *endptr = NULL;
+ 
+	// Check the string
+	long input = 0;
+	strptr = argv[1];
+	input = strtol(strptr, &endptr, 10);
+ 
+ 
+	if (endptr == strptr || *endptr != '\0') {
+		printf("Wront input string\n");
+		return -3;
+	}
+ 
+	if (input <= 0) {
+		printf("The number must be greater then 0\n");
+		return -4;
+	}
+ 
+	if (errno == ERANGE && (input == LONG_MAX || input == LONG_MIN)) {
+		printf("Out of range\n");
+		return -5;
+	}
+
+	return input;
 }
